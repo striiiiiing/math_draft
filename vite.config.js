5f5446edd5c16afa14f73bf026371480ced4a7b8
striiing <awellwell@foxmail.com>
2026-02-26 23:09:55 +0800

    feat:添加用于Github构建网页相关

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: '/math_draft/', // <-- 加上这一行！记得改成实际的仓库名
   plugins: [
     vue({
       template: {
